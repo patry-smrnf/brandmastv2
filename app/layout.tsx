ea7b2eefe6b57ff9cd68c55d5d0437ddd3fa0f60
Patry <patrykstepien05@gmail.com>
2025-10-16 12:29:58 +0100

    Fixed translation error

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+  <html lang="en" translate="no">    
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <Toaster richColors position="top-center" />
